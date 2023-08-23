f65d49e64c1bd3191be9db93869b29d35acb6282
Denis Radchenko <denis.radchenko@cauri.com>
2023-08-23 10:52:35 +0300

    Перенос статики

--- Magazin/settings.py
+++ Magazin/settings.py
@@ -121,17 +121,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'static'
-
 STATIC_URL = '/static/'
+
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
 # ]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 # LOGGING = {
--- Magazin/urls.py
+++ Magazin/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('', include('pervi_sait.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
